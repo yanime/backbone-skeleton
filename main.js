59f2cd48f7e15f87420cd5aa3004f78a2594e4d1
yugioh <yugioh852@gmail.com>
2015-03-17 00:04:27 +0200

    Add backbone dependencies and create require config

---
+++ main.js
@@ -0,0 +1,27 @@
+require.config({
+    paths: {
+        jquery: 'bower_components/jquery/dist/jquery',
+        underscore: 'bower_components/underscore/underscore',
+        backbone: 'bower_components/backbone/backbone'
+    },
+    shim: {
+        "underscore": {
+            deps: [],
+            exports: "_"
+        },
+        "backbone": {
+            deps: ["jquery", "underscore"],
+            exports: "Backbone"
+        }
+    }
+
+});
+
+define(["jquery", "underscore", "backbone"],
+    function ($, _, Backbone) {
+        console.log("Test output");
+        console.log("$: " + typeof $);
+        console.log("_: " + typeof _);
+        console.log("Backbone: " + typeof Backbone);
+    }
+);
